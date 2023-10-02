163da6b7c8a3a3106a20b6e5a4e51ae21b9d416a
Bandaanilreddy <bandaanilreddy@gmail.com>
2023-10-02 19:34:10 +0530

    added 3 new models in new model file

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,13 @@
 print('This is a model file')
+
+
+def model1():
+    print('Adding code for ML model1')
+
+
+def model2():
+    print('Adding code for ML model2')
+
+def model3():
+    print('Adding code for ML model3')
+
